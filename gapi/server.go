6ef17dbf1c56fff5e6fe888b75192026bc1efd2f
Giuliano Gabella <ggabella91@gmail.com>
2023-07-09 13:49:35 -0500

    add grpc api, proto files, and auto-generated protobuf go files; add swagger docs json

---
+++ gapi/server.go
@@ -0,0 +1,34 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/ggabella91/simplebank/db/sqlc"
+	"github.com/ggabella91/simplebank/pb"
+	"github.com/ggabella91/simplebank/token"
+	"github.com/ggabella91/simplebank/util"
+)
+
+// Server serves gRPC requests for our banking service
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+// NewServer creates a new gRPC server
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %d", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	return server, nil
+}
