47945a012069d40b710592f2aad4ce4d9ee84427
Giuliano Gabella <ggabella91@gmail.com>
2023-08-11 17:12:51 -0500

    Integrate workers to web server

--- Makefile
+++ Makefile
@@ -55,4 +55,7 @@ proto:
 evans:
 	evans --host localhost --port 9090 -r repl
 
-.PHONY: network postgres createdb dropdb migrateup migratedown db_docs db_schema migrateup1 migratedown1 sqlc test server mock proto evans
+redis:
+	docker run --name redis -p 6379:6379 -d redis:7-alpine
+
+.PHONY: network postgres createdb dropdb migrateup migratedown db_docs db_schema migrateup1 migratedown1 sqlc test server mock proto evans redis
--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,11 +2,14 @@ package gapi
 
 import (
 	"context"
+	"time"
 
 	db "github.com/ggabella91/simplebank/db/sqlc"
 	"github.com/ggabella91/simplebank/pb"
 	"github.com/ggabella91/simplebank/util"
 	"github.com/ggabella91/simplebank/val"
+	"github.com/ggabella91/simplebank/worker"
+	"github.com/hibiken/asynq"
 	"github.com/lib/pq"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
@@ -40,6 +43,19 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 			}
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	taskPayload := &worker.PayloadSendVerifyEmail{
+		Username: arg.Username,
+	}
+	opts := []asynq.Option{
+		asynq.MaxRetry(10),
+		asynq.ProcessIn(10 * time.Second),
+		asynq.Queue(worker.QueueCritical),
+	}
+	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
 	}
 
 	rsp := &pb.CreateUserResponse{
